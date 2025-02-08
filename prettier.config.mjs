41b871e78b0903572ad1070fd9641f2caeaf10c1
HTLA380 <htetlin099798@gmail.com>
2025-02-08 18:48:21 +0630

    initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,22 @@
+export default {
+	arrowParens: "always",
+	bracketSpacing: true,
+	htmlWhitespaceSensitivity: "css",
+	insertPragma: false,
+	bracketSameLine: true,
+	jsxSingleQuote: false,
+	printWidth: 120,
+	proseWrap: "preserve",
+	quoteProps: "as-needed",
+	requirePragma: false,
+	semi: true,
+	singleQuote: false,
+	tabWidth: 2,
+	trailingComma: "none",
+	useTabs: true,
+	endOfLine: "auto",
+	plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+	importOrder: ["^react$", "^next(/.*)?$", "next-themes", "@/*", "<THIRD_PARTY_MODULES>"],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const page = () => {
+	return <div className="text-red-500">This is a home page</div>;
+};
+
+export default page;
